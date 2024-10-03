b4aaa3fbe526c50f0773f2439ba004178738847b
youssef nagib <youssefnagib14@yahoo.com>
2024-10-03 20:03:25 +0300

    class

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,54 @@
+/* eslint-disable no-underscore-dangle */
+/*
+onstructor attributes:
+amount (Number)
+currency (Currency)
+Each attribute must be stored in an “underscore” attribute version (ex: name is stored in _name)
+Implement a getter and setter for each attribute.
+Implement a method named displayFullPrice that
+returns the attributes in the following format amount currency_name (currency_code).
+Implement a static method named convertPrice.
+It should accept two arguments: amount (Number), conversionRate (Number).
+The function should return the amount multiplied by the conversion rate.
+*/
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    if (typeof amount !== 'number') {
+      throw new Error('Amount must be a number');
+    }
+    this._amount = amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    if (!(currency instanceof Currency)) {
+      throw new Error('Currency must be a currency');
+    }
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+      throw new Error('Both amount and conversionRate must be numbers');
+    }
+    return amount * conversionRate;
+  }
+}
