3ba45bbe8f44276e23f13eb15088180c1a5fbf77
youssef nagib <youssefnagib14@yahoo.com>
2024-10-03 05:09:22 +0300

    promise

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -2,18 +2,13 @@ import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
   let res = {};
+
   try {
     const photo = await uploadPhoto();
     const user = await createUser();
-    res = {
-      photo,
-      user,
-    };
+    res = { photo, user };
   } catch (err) {
-    res = {
-      photo: null,
-      user: null,
-    };
+    res = { photo: null, user: null };
   }
   return res;
 }
