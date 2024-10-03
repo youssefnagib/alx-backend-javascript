049c7558cb4bc4c52e16fb025d46599a22fca137
youssef nagib <youssefnagib14@yahoo.com>
2024-10-03 18:36:40 +0300

    class

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,44 @@
+/* eslint-disable no-underscore-dangle */
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName === 'string') {
+      this._name = newName;
+    } else {
+      throw new Error('Name must be a string');
+    }
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(newLength) {
+    if (typeof newLength === 'number') {
+      this._length = newLength;
+    } else {
+      throw new Error('Length must be a number');
+    }
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    if (Array.isArray(newStudents)) {
+      this._students = newStudents;
+    } else {
+      throw new Error('Students must be an array');
+    }
+  }
+}
