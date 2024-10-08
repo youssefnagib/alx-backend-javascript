01fe4dace43f337323bc1fa9f594de5be8361b1d
youssef nagib <youssefnagib14@yahoo.com>
2024-10-08 12:04:21 +0300

    DataView

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,4 +1,7 @@
 export default function createInt8TypedArray(length, position, value) {
+  if (position >= length || position < 0) {
+    throw new Error('Position outside range');
+  }
   const buffer = new ArrayBuffer(length);
   const view = new DataView(buffer);
 
