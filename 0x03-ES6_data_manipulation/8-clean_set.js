4b58cc12693e04ff98fdf9a7bf930d3f4f57c338
youssef nagib <youssefnagib14@yahoo.com>
2024-10-08 14:50:01 +0300

    startwith

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -8,7 +8,7 @@ export default function cleanSet(set, startString) {
   const result = [];
   if (startString) {
     set.forEach((value) => {
-      if (value.startsWith(startString)) {
+      if (typeof value === 'string' && value.startsWith(startString)) {
         result.push(value.slice(startString.length));
       }
     });
