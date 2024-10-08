902a28a951cbc0268369f522e493e83ef5fae683
youssef nagib <youssefnagib14@yahoo.com>
2024-10-08 14:46:56 +0300

    startwith

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,10 @@
 export default function cleanSet(set, startString) {
+  if (
+    !set || !startString || !(set instanceof Set) || typeof startString !== 'string'
+  ) {
+    return '';
+  }
+
   const result = [];
   if (startString) {
     set.forEach((value) => {
