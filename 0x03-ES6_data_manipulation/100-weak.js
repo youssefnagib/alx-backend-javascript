ce4cbd72fe01f159b16a0544aed7499acf50e8c5
youssef nagib <youssefnagib14@yahoo.com>
2024-10-08 16:40:13 +0300

    api

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -6,9 +6,9 @@ export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
     weakMap.set(endpoint, 0);
   }
-  const queryCount = weakMap.get(endpoint);
-  if (queryCount >= MAX_ENDPOINT_CALLS) {
+
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  if (weakMap.get(endpoint) >= MAX_ENDPOINT_CALLS) {
     throw new Error('Endpoint load is high');
   }
-  weakMap.set(endpoint, queryCount + 1);
 }
