c557224338c0fa9f746ef424278d7281560da646
youssef nagib <youssefnagib14@yahoo.com>
2024-11-28 19:49:22 +0200

    node

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,29 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./5-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  let spy;
+
+  beforeEach(() => {
+    if (!spy) {
+      spy = sinon.spy(console);
+    }
+  });
+
+  afterEach(() => {
+    spy.log.resetHistory();
+  });
+
+  it('sendPaymentRequestToApi(100, 20) logs "The total is: 120" to the console', () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(spy.log.calledWith('The total is: 120')).to.be.true;
+    expect(spy.log.calledOnce).to.be.true;
+  });
+
+  it('sendPaymentRequestToApi(10, 10) logs "The total is: 20" to the console', () => {
+    sendPaymentRequestToApi(10, 10);
+    expect(spy.log.calledWith('The total is: 20')).to.be.true;
+    expect(spy.log.calledOnce).to.be.true;
+  });
+});
