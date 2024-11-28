fadb584ae685e3e535601434cab852788252d411
youssef nagib <youssefnagib14@yahoo.com>
2024-11-28 18:17:26 +0200

    node

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -6,6 +6,13 @@ describe('calculateNumber', () => {
     assert.strictEqual(calculateNumber(1, 3), 4);
   });
 
+  it('should return 4 when inputs is floating are 1.0 and 3.0', () => {
+    assert.strictEqual(calculateNumber(1.0, 3.0), 4);
+  });
+  it('should return 4 when inputs is floating are 1.4 and 3.4', () => {
+    assert.strictEqual(calculateNumber(1.4, 3.4), 4);
+  });
+
   it('should return 5 when inputs are 1 and 3.7', () => {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
   });
@@ -24,5 +31,9 @@ describe('calculateNumber', () => {
 
   it('should handle floating point correctly', () => {
     assert.strictEqual(calculateNumber(0.49999, 0.5), 1);
+  });
+
+  it('should handle floating point correctly if input 1.4999999 +1.499999 return 2', () => {
+    assert.strictEqual(calculateNumber(1.499999, 1.49999), 2);
   });
 });
